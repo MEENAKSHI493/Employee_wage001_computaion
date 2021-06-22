c36c1efd71dd41211b2b1d9cd78abc5712b3c785
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-23 02:12:41 +0530

    emlpoyee wage computaion

---
+++ src/javaemployeewagecomputation/javaemployeecomputation.java
@@ -0,0 +1,10 @@
+package javaemployeewagecomputation;
+
+public class javaemployeecomputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module javaemployeewagecomputation1 {
+}
